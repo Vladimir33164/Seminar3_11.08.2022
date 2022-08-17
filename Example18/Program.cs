065b9dc7e7b9fbe897f532f0d5beb8dcc8cb53cc
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-17 23:30:35 +0300

    задача17. по номеру четверти определение координат точки в плоскости

---
+++ Example18/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 18: Напишите программу, которая по 
+//заданному номеру четверти, показывает диапазон 
+//возможных координат точек в этой четверти (x и y).
+
+int qarter = Convert.ToInt32(Console.ReadLine());
+if (qarter == 1) Console.WriteLine("x > 0 и y > 0");
+if (qarter == 2) Console.WriteLine("x < 0 и y > 0");
+if (qarter == 3) Console.WriteLine("x < 0 и y < 0");
+if (qarter == 4) Console.WriteLine("x > 0 и y < 0");
