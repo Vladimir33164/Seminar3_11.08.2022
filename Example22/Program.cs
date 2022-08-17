28a263a40f9200b987358432c613bf96a637775d
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-18 01:06:19 +0300

    задача22. на вход N, на выходе таблица квадратов чисел

---
+++ Example22/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 22: Напишите программу, которая 
+//принимает на вход число (N) и выдаёт таблицу 
+//квадратов чисел от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+int N = Convert.ToInt32(Console.ReadLine());
+//int start = 1;
+//while (start <= N)
+//{
+    //Console.WriteLine(Math.Pow(start,2));
+    //start++;
+//}
+for (int start = 1; start <= N; start++)
+{
+    Console.WriteLine(Math.Pow(start,2));
+}
